83bf17803aa3d794bcc292b1d4204c112e45e3a6
Prince Arya <prince422005@gmail.com>
2026-02-10 18:13:20 +0530

    place repo need fix

--- src/main/java/com/prince/repository/PlaceRepository.java
+++ src/main/java/com/prince/repository/PlaceRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.prince.model.Place;
 import com.prince.model.enums.Category;
@@ -18,8 +19,10 @@ public interface PlaceRepository extends JpaRepository<Place, Long>{
     Page<Place> findByCategoryAndActiveTrue(Category category, Pageable pageable);
 
     //custom jpql to search in desc. or name
+    //calling lower hinders performance,,
+    //  need fix
     @Query("SELECT p FROM Place p WHERE p.active = true AND " +
            "(LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')))")
-    Page<Place> searchPlaces(String keyword, Pageable pageable);
+    Page<Place> searchPlaces(@Param("keyword") String keyword, Pageable pageable);
 }
