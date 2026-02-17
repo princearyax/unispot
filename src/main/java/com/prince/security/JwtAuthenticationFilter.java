259f177d33f6ff740ddd6db408532d861500b823
Prince Arya <prince422005@gmail.com>
2026-02-17 15:01:34 +0530

    auth filter , security context

--- src/main/java/com/prince/security/JwtAuthenticationFilter.java
+++ src/main/java/com/prince/security/JwtAuthenticationFilter.java
@@ -3,7 +3,11 @@ package com.prince.security;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter; //so that one api call just executes it once, no performance bottleneck
 
@@ -29,13 +33,46 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
     )throws ServletException, IOException {
 
         final String authHeader = request.getHeader("Authorization");
-        String token = null;
-        if(authHeader != null && authHeader.startsWith("Bearer ")){
-            token = authHeader.substring(7);
+        final String userEmail;
+        final String jwtToken;
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return; //stop executing rest of filter
         }
-        if(token == null){
+
+        jwtToken = authHeader.substring(7);
+        userEmail = jwtUtils.extractUsername(jwtToken);
+
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){ //we have email but unauthenticated user
+            //fetch user from db, userDetailsService, gonna enforce account status too
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+            //validate token
+            if(jwtUtils.validateToken(jwtToken, userDetails.getUsername())){
+                //create official SpringSecurityAuth Token
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,   //password is null as jwt based
+                    userDetails.getAuthorities() //roles e.g. Role_Admin
+                );
+
+                //extra details, ip add. session id. for audit/logging
+                authToken.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                //now putting it in securityContext so taht rest appln can know
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+
         }
+
+        //pass to next filter
+        filterChain.doFilter(request, response);
         
     }
-
 }
+
+
+// UsernamePasswordAuthenticationToken is the spring's official id card like, we take USerDetails inside, hand it to SecurityContextholder(which hold user for entire http req)
