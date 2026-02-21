67f3d3d68bab3faff89cc98c42cf0cfc7af47957
Prince Arya <prince422005@gmail.com>
2026-02-21 20:53:54 +0530

    auth service interface

---
+++ src/main/java/com/prince/service/AuthService.java
@@ -0,0 +1,10 @@
+package com.prince.service;
+
+import com.prince.dto.request.RegisterRequest;
+import com.prince.dto.response.AuthResponse;
+
+//interface for service, so that can use mockito w/o hitting real db
+public interface AuthService {
+    AuthResponse register(RegisterRequest request);
+    AuthResponse login(AuthResponse response);
+}
