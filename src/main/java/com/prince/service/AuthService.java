ba951b2f2cd2458dd8c1266c6aa8bbe2406b2eda
Prince Arya <prince422005@gmail.com>
2026-02-22 16:38:47 +0530

    auth service fix , and impl

--- src/main/java/com/prince/service/AuthService.java
+++ src/main/java/com/prince/service/AuthService.java
@@ -1,10 +1,11 @@
 package com.prince.service;
 
+import com.prince.dto.request.LoginRequest;
 import com.prince.dto.request.RegisterRequest;
 import com.prince.dto.response.AuthResponse;
 
 //interface for service, so that can use mockito w/o hitting real db
 public interface AuthService {
     AuthResponse register(RegisterRequest request);
-    AuthResponse login(AuthResponse response);
+    AuthResponse login(LoginRequest request);
 }
---
+++ src/main/java/com/prince/service/implementation/AuthSeerviceImpl.java
@@ -0,0 +1,59 @@
+package com.prince.service.implementation;
+
+import java.util.Set;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.prince.dto.request.LoginRequest;
+import com.prince.dto.request.RegisterRequest;
+import com.prince.dto.response.AuthResponse;
+import com.prince.model.User;
+import com.prince.model.enums.Role;
+import com.prince.model.enums.UserStatus;
+import com.prince.repository.UserRepository;
+import com.prince.security.JwtUtils;
+import com.prince.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthSeerviceImpl implements AuthService{
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    @Transactional //ensures db operations are atomic
+    public AuthResponse register(RegisterRequest request) {
+        //if user already exists with the mail
+        if(userRepository.existsByEmail(request.email())){
+            //sud be custom exception handled by globalHandler
+            throw new IllegalArgumentException("Email is already taken");
+        }
+
+        //map dto to entity and hash the password
+        User user = User.builder()
+                        .email(request.email())
+                        .password(passwordEncoder.encode(request.password()))
+                        .status(UserStatus.PENDING)
+                        .roles(Set.of(Role.ROLE_USER))
+                        .build();
+        
+        //save to db
+        userRepository.save(user);
+
+        return new AuthResponse(null, "Wait for approval");
+    }
+
+    @Override
+    public AuthResponse login(LoginRequest request) {
+        throw new UnsupportedOperationException("Unimplemented method 'login'");
+    }
+
+}
