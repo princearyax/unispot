c7f81c2fe9d192361db1d5e0ac7e64b39b821ca9
Prince Arya <prince422005@gmail.com>
2026-08-01 14:24:24 +0530

    initial maven

---
+++ src/main/java/com/prince/unispot/UnispotApplication.java
@@ -0,0 +1,13 @@
+package com.prince.unispot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnispotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnispotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prince/unispot/UnispotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prince.unispot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnispotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
