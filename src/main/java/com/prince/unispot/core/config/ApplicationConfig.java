d3766e0cc8953a6c061c4d29d74c8827771fb005
Prince Arya <prince422005@gmail.com>
2026-08-14 13:54:22 +0530

    security config beans

---
+++ src/main/java/com/prince/unispot/core/config/ApplicationConfig.java
@@ -0,0 +1,58 @@
+//not using securityConfig as can create circular dependency, 
+package com.prince.unispot.core.config;
+
+import com.prince.unispot.user.infrastructure.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collections;
+
+//will define, fetching user, UserDetailsService, and hash passweord, etc
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            com.prince.unispot.user.domain.model.User user = userRepository.findByEmail(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            
+            //mapping our user to the user spring can understand
+            return new User(
+                    user.getId().toString(), //principal will be id
+                    user.getPasswordHash(),
+                    Collections.emptyList() //jwt is used for roles
+            );
+        };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager(); //taking hold
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); //using bcrypt
+    }
+}
