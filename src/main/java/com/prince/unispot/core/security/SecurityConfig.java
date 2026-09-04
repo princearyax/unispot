c833ef4e8eaac6b86c02c7a772aca8ca8db28831
Prince Arya <prince422005@gmail.com>
2026-09-04 17:19:58 +0530

    cors added

--- src/main/java/com/prince/unispot/core/security/SecurityConfig.java
+++ src/main/java/com/prince/unispot/core/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.prince.unispot.core.security;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,6 +14,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration //modrn componrnt based, explicitly build and return SecurityFilterChain bean
@@ -24,9 +30,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            //sud ensure cors policy to protect refresh route tho
-            //set some allowed methods/origin/headers etc in corsConfigurationSource()
-
+            //cors added
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             
             //well jwt immune so
             .csrf(AbstractHttpConfigurer::disable)
@@ -47,5 +52,17 @@ public class SecurityConfig {
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); // frontend origin for cors
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
